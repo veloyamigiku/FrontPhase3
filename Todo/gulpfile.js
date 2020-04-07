e5e317522415e648dbc37d0a05bccdd24a39f9e3
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2020-04-07 07:16:44 +0000

    ・スタイルシートの変更を監視する。 ・TODO入力領域にスタイルを追加する。

--- Todo/gulpfile.js
+++ Todo/gulpfile.js
@@ -2,13 +2,26 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
 
 gulp.task('sass', function(done) {
     gulp
         .src('sass/custom.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(sourcemaps.write("./"))
         .pipe(gulp.dest('css'));
     done();
+});
+
+gulp.task('sass-watch', function(done) {
+    gulp
+        .watch(
+            'sass/custom.scss',
+            gulp.series('sass'));
 });
